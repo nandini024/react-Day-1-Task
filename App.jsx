ce1c6ce25dbaba874acc075e150e06c30acf5d64
nandini024 <108275550+nandini024@users.noreply.github.com>
2025-04-24 13:50:56 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+import Day1 from "./Components/Day1/day1"
+import Day2 from "./Components/Day2/class.comp"
+
+
+
+function App()
+{
+return(
+ <>
+ {/* <Day1/> */}
+ <Day2/>
+
+
+ </>
+)
+}
+
+export default App
---
+++ Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css"
+function Navbar()
+{
+return(
+    <div id='nav_container'>
+        <h2>NAVBAR Solutions</h2>
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+    </div>
+)
+
+}
+export default Navbar
