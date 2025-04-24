d9aa65ffa3bea646a24026ecb9dca10ed2bbbbe5
nandini024 <108275550+nandini024@users.noreply.github.com>
2025-04-24 13:46:09 +0530

    Add files via upload

---
+++ navbar.jsx
@@ -0,0 +1,19 @@
+
+function Navbar()
+{
+    return(
+        <div className="container">
+            <h1>Landy</h1>
+            <div className="navcontainer"><ul>
+                <li>About</li>
+                <li>Mission</li>
+                <li>Product</li>
+                <li className="contact">Contact</li>
+            </ul></div>
+
+        </div>
+
+
+    )
+}
+export default Navbar
